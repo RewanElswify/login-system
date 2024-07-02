780a82585d3dd1bd19895b3b190dda9c2c6cb156
RewanElswify <rewanwahed93@gmail.com>
2024-07-03 01:50:25 +0300

    Delete home directory

--- home/js/index.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('userName').innerHTML = localStorage.getItem('userName');
-
-
-document.getElementById('logBtn').addEventListener('click', function () {
-    localStorage.removeItem('userName');
-})
-
